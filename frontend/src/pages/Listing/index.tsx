11e396e4e0a9b19ad5aee0672874abe99f785ba5
Ravi Rocha <ravigprocha@gmail.com>
2022-01-11 00:23:57 +0044

    MovieCard

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,10 +1,37 @@
+import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
 
 function Listing() {
 
     return (
-        <Pagination />
-    );
+        <>
+            <Pagination />
+
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm6 col-lg4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm6 col-lg4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm6 col-lg4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm6 col-lg4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm6 col-lg4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+
+                </div>
+
+            </div>
+
+
+        </>
+    )
 }
 
 export default Listing;
